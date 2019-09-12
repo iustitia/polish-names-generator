8421254acb96a3d99dc748bc59fd0b6d858e6f1b
iustitia <jkubisztal@gmail.com>
2019-09-12 15:43:59 +0200

    make it more readable

--- grades.py
+++ grades.py
@@ -8,12 +8,14 @@ for student in gen_names(350, formatted=False):
     students.append([student[0], random.randint(200000, 500000), grades])
 
 for s in students:
-    print('{} | {} | {}'.format(s[0], s[1], s[2]))
+    print('{:15s} | {} | {}'.format(s[0], s[1], s[2]))
 
 input('Press any key to start calculations')
-
 print('run')
 
 for s in students:
-    avg = sum(s[2])/len(s[2])
-    print(s[0], avg)
+    grades_sum = sum(s[2])
+    grades_count = len(s[2])
+    avg = grades_sum/grades_count
+
+    print('{:15s} | {} | {}'.format(s[0], s[1], avg))
